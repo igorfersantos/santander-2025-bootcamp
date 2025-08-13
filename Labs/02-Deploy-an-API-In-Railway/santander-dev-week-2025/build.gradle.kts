75e3c74212c9606d63b3c1dcab8260c5d5995dcd
Igor Fernandes <igorfernandes@paprikagames.com>
2025-08-13 16:03:56 -0300

    fix: Changed the gradle build settings again

--- Labs/02-Deploy-an-API-In-Railway/santander-dev-week-2025/build.gradle.kts
+++ Labs/02-Deploy-an-API-In-Railway/santander-dev-week-2025/build.gradle.kts
@@ -10,9 +10,6 @@ version = "0.0.1-SNAPSHOT"
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
-//    toolchain {
-//        vendor = JvmVendorSpec.ADOPTIUM
-//    }
 }
 
 repositories {
@@ -23,17 +20,22 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+    implementation("org.modelmapper:modelmapper:3.2.0")
+    
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
+    
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+springBoot {
+    mainClass.set("br.com.igorfernandes.santander.Application")
+}
+
 tasks.bootJar {
     archiveFileName.set("app.jar")
-    manifest {
-        attributes("Main-Class" to "br.com.igorfernandes.santander.Application")
-    }
+    mainClass.set("br.com.igorfernandes.santander.Application")
     launchScript()
 }
 
